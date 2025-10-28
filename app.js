eeceea62b8eee1c1da5a5dc684dc08e906672209
Your Name <you@example.com>
2025-10-28 14:26:21 +0700

    first commit

--- app.js
+++ app.js
@@ -2,13 +2,66 @@
 let Name = document.getElementById("Name")
 let package = document.getElementById("package")
 let speed = document.getElementById("speed")
+let btn = document.getElementById("callout")
 
-let Trueinternet = {
-    Name: "300 บาท 1 Mbps",
-    speed: ' เน็ตความเร็ว 15 Mbps',
-    number: "*900*3719*17331647#"
+btn.onclick = () => {
+    window.location.href = "tel:*900*3719*17331647#";
 }
 
-Name.innerHTML = Trueinternet.Name
-package.innerHTML = Trueinternet.speed
-speed.innerHTML = Trueinternet.number
+let btn2day = document.getElementById("btn-2day")
+let btn3day = document.getElementById("btn-3day")
+let btn7day = document.getElementById("btn-7day")
+let btn30day = document.getElementById("btn-30day")
+
+let twoday = document.getElementById("twoday")
+let threeday = document.getElementById("threeday")
+let sevenday = document.getElementById("sevenday")
+let thirtyday = document.getElementById("thirty")
+
+
+btn2day.onclick = () => {
+  twoday.hidden = false
+  threeday.hidden = true
+  sevenday.hidden = true
+  thirtyday.hidden = true
+}
+
+btn3day.onclick = () => {
+  twoday.hidden = true
+  threeday.hidden = false
+  sevenday.hidden = true
+  thirtyday.hidden = true
+}
+
+btn7day.onclick = () => {
+  twoday.hidden = true
+  threeday.hidden = true
+  sevenday.hidden = false
+}
+
+btn30day.onclick = () => {
+  twoday.hidden = true
+  threeday.hidden = true
+  sevenday.hidden = true
+  thirtyday.hidden = false
+}
+
+
+
+// let Trueinternet = {
+//     Name: "300 บาท 1 Mbps",
+//     speed: ' เน็ตความเร็ว 15 Mbps',
+//     number: "*900*3719*17331647#"
+// }
+
+// let Trueinterne = {
+//     Name: "300 บาท 1 Mbps",
+//     speed: ' เน็ตความเร็ว 15 Mbps',
+//     number: "*900*3719*17331647#"
+// }
+
+// Name.innerHTML = Trueinternet.Name
+// package.innerHTML = Trueinternet.speed
+// speed.innerHTML = Trueinternet.number
+
+
