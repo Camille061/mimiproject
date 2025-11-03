9983124670182cfd0072317d18e13d37439255e2
Your Name <you@example.com>
2025-11-03 17:42:47 +0700

    first commit

--- app.js
+++ app.js
@@ -8,18 +8,28 @@ let btn = document.getElementById("callout")
 //     //  window.location.href = 'tel:*900*3719*17331647#'
 // }
 
+let btn1day = document.getElementById("btn-1day")
 let btn2day = document.getElementById("btn-2day")
 let btn3day = document.getElementById("btn-3day")
 let btn7day = document.getElementById("btn-7day")
 let btn30day = document.getElementById("btn-30day")
 
+let oneday = document.getElementById("oneday")
 let twoday = document.getElementById("twoday")
 let threeday = document.getElementById("threeday")
 let sevenday = document.getElementById("sevenday")
 let thirtyday = document.getElementById("thirty")
 
+btn1day.onclick = () => {
+  oneday.hidden = false
+  twoday.hidden = true
+  threeday.hidden = true
+  sevenday.hidden = true
+  thirtyday.hidden = true
+}
 
 btn2day.onclick = () => {
+  oneday.hidden = true
   twoday.hidden = false
   threeday.hidden = true
   sevenday.hidden = true
@@ -27,6 +37,7 @@ btn2day.onclick = () => {
 }
 
 btn3day.onclick = () => {
+  oneday.hidden = true
   twoday.hidden = true
   threeday.hidden = false
   sevenday.hidden = true
@@ -34,6 +45,7 @@ btn3day.onclick = () => {
 }
 
 btn7day.onclick = () => {
+  oneday.hidden = true
   twoday.hidden = true
   threeday.hidden = true
   sevenday.hidden = false
@@ -41,6 +53,7 @@ btn7day.onclick = () => {
 }
 
 btn30day.onclick = () => {
+  oneday.hidden = true
   twoday.hidden = true
   threeday.hidden = true
   sevenday.hidden = true
